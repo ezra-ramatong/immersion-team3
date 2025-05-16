330c7c7767cf7d06d94ab4417b1774fbb850a005
Gizmo10 <sw.ndzala@gmail.com>
2025-05-16 20:02:22 +0200

    Added user landing page and form to enter their details with CSS styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import "./styles/style.css";
+
+const takeQuizzButton = document.querySelector(".start-quiz");
+const landingPageSection = document.querySelector(".welcome-intro");
+
+takeQuizzButton.onclick = () => {
+    landingPageSection.classList.toggle('inactive-intro');
+};
+
