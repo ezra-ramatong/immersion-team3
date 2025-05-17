828e9cf2595596f7131a6b6e0965cfcbda6d4101
Gizmo10 <sw.ndzala@gmail.com>
2025-05-17 10:40:28 +0200

    Class model for the user

---
+++ src/models/user_model.js
@@ -0,0 +1,99 @@
+class User {
+  #firstName;
+  #lastName;
+  #userName;
+  #score = 0;
+  #numQuestions = 0;
+  #timePerQuestion = 0;
+  #category;
+  #questions = [];
+  #currentQuestionIndex = 0;
+  #highScores = [];
+
+  constructor(firstName, lastName, userName, category) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#userName = userName;
+    this.#category = category;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  get userName() {
+    return this.#userName;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  get numQuestions() {
+    return this.#numQuestions;
+  }
+
+  get timePerQuestion() {
+    return this.#timePerQuestion;
+  }
+
+  get category() {
+    return this.#category;
+  }
+
+  get questions() {
+    return this.#questions;
+  }
+
+  get currentQuestionIndex() {
+    return this.#currentQuestionIndex;
+  }
+
+  get highScores() {
+    return this.#highScores;
+  }
+
+  set firstName(name) {
+    this.#firstName = name;
+  }
+
+  set lastName(name) {
+    this.#lastName = name;
+  }
+
+  set userName(username) {
+    this.#userName = username;
+  }
+
+  set category(category){
+    this.#category = category;
+  }
+
+  set score(score){
+    this.#score = score;
+  }
+
+  set numQuestions(numQuestions){
+    this.#numQuestions = numQuestions;
+  }
+
+  set timePerQuestion(timePerQuestion){
+    this.#timePerQuestion = timePerQuestion;
+  }
+
+  set questions(questions){
+    this.#questions = questions;
+  }
+
+  set currentQuestionIndex(index){
+    this.#currentQuestionIndex = index;
+  }
+
+  set highScores(highScores){
+    this.#highScores = highScores;
+  }
+}
